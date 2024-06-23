393108bb83eb2670e1ade332a05ae557e9b4ec98
NatnaleKete <ketemanatnael023@gmail.com>
2024-06-23 08:08:44 -0700

    1-getMe controller function added

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -3,15 +3,18 @@ import {
   signup,
   login,
   logout,
+  getMe,
   forgotPassword,
   resetPassword,
 } from "../controller/auth.controller.js";
+import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.get("/me", protect, getMe);
 router.post("/forgotPassword", forgotPassword);
 router.patch("/resetPassword/:token", resetPassword);
 
